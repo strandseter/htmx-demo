ce19ac94b04fcef31a0054d2e84d94f75d354fbb
Anders Strandseter <anders@lyonfuture.no>
2023-11-23 13:15:00 +0100

    feat: list and add todos

---
+++ src/data/todos.ts
@@ -0,0 +1,1 @@
+export const FAKE_TODOS_TABLE: string[] = [];
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
+import bodyParser from "body-parser";
 import express from "express";
 import path from "path";
+import reload from "reload";
+import { FAKE_TODOS_TABLE } from "./data/todos";
 
 const app = express();
 const port = 3000;
@@ -8,10 +11,30 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.static("node_modules"));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.render(path.join(__dirname, "../src/views/index"));
+});
+
+app.get("/todos", (req, res) => {
+  res.render(path.join(__dirname, "../src/views/todos"), {
+    todos: FAKE_TODOS_TABLE,
+  });
+});
+
+app.post("/todos/create", (req, res) => {
+  const todo = req.body.todo;
+
+  FAKE_TODOS_TABLE.push(todo);
+
+  res.render(path.join(__dirname, "../src/views/todos"), {
+    todos: [todo],
+  });
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+reload(app);
